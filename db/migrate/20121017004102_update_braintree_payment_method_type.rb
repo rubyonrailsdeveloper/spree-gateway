b46d44de575781b848d79d14fa9e896f8738f6af
Greenfield <webdeveloper1001@outlook.com>
2019-05-06 22:48:00 +0800

    Update 20121017004102_update_braintree_payment_method_type.rb

--- db/migrate/20121017004102_update_braintree_payment_method_type.rb
+++ db/migrate/20121017004102_update_braintree_payment_method_type.rb
@@ -3,7 +3,5 @@ class UpdateBraintreePaymentMethodType < SpreeExtension::Migration[4.2]
     Spree::PaymentMethod.where(:type => "Spree::Gateway::Braintree").update_all(:type => "Spree::Gateway::BraintreeGateway")
   end
 
-  def down
-    Spree::PaymentMethod.where(:type => "Spree::Gateway::BraintreeGateway").update_all(:type => "Spree::Gateway::Braintree")
-  end
+  
 end
